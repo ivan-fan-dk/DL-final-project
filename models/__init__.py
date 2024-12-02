c982e3ba3fb2751b3bb8a5adae66c331791ee83f
han-daniel <43096627+han-daniel@users.noreply.github.com>
2024-12-02 16:36:47 -0500

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .DispNetS import DispNetS
-from .SE_DispNetS import SE_DispNetS, SEBottleneck
+from .SE_DispNetS import SEBottleneck
+from .SE_DispNetS import SE_DispNetS
 from .PoseExpNet import PoseExpNet
